e9be4b19f8af5afa52731bcb2dd4498e2dace9ba
has5aan <has5aan@outlook.com>
2021-03-08 12:20:19 +0100

    Modified Documentation

--- src/Router.php
+++ src/Router.php
@@ -5,7 +5,7 @@ namespace Voie;
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-set_error_handler(function($errno , $errstr , $errfile , $errline) {
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
     throw new Exception(sprintf("%s(%s):%s", $errfile, $errline, $errstr), 500);
 }, E_ALL);
 
@@ -18,7 +18,7 @@ use Voie\PipelineService\ErrorHandlerInterface;
 use Voie\PipelineService\MiddlewareInterface;
 
 /**
- * Provides a request routing infrastructure through an object instance analogous to HTTP verbs.
+ * Provides an infrastructure to route requests analogous to HTTP request methods through an instance object.
  *
  * Allows registering different route handlers. Each route handler responds to a route path and a specific
  * request method. Route handlers against a request method can be registered using, get(), put(), patch(), post()
@@ -84,7 +84,8 @@ class Router extends stdClass
     private array $services;
 
     /** Initializes the internal data structures to maintain routes, middleware and error handlers. */
-    public function __construct() {
+    public function __construct()
+    {
         $this->routes = array();
         $this->initializers = array();
         $this->middleware = array();
@@ -100,7 +101,7 @@ class Router extends stdClass
      * @param Closure $requestHandler Defines the logic to handle the Http request.
      * @return Route Route object added to the route collection.
      */
-    public function get(string $routePath, Closure $requestHandler) : Route
+    public function get(string $routePath, Closure $requestHandler): Route
     {
         return $this->addRoute($routePath, $requestHandler, 'GET');
     }
@@ -111,7 +112,7 @@ class Router extends stdClass
      * @param Closure $requestHandler Defines the logic to handle the Http request.
      * @return Route Route object added to the route collection.
      */
-    public function put(string $routePath, Closure $requestHandler) : Route
+    public function put(string $routePath, Closure $requestHandler): Route
     {
         return $this->addRoute($routePath, $requestHandler, 'PUT');
     }
@@ -122,7 +123,7 @@ class Router extends stdClass
      * @param Closure $requestHandler Defines the logic to handle the Http request.
      * @return Route Route object added to the route collection.
      */
-    public function patch(string $routePath, Closure $requestHandler) : Route
+    public function patch(string $routePath, Closure $requestHandler): Route
     {
         return $this->addRoute($routePath, $requestHandler, 'PATCH');
     }
@@ -133,7 +134,7 @@ class Router extends stdClass
      * @param Closure $requestHandler Defines the logic to handle the Http request.
      * @return Route Route object added to the route collection.
      */
-    public function post(string $routePath, closure $requestHandler) : Route
+    public function post(string $routePath, closure $requestHandler): Route
     {
         return $this->addRoute($routePath, $requestHandler, 'POST');
     }
@@ -144,19 +145,19 @@ class Router extends stdClass
      * @param Closure $requestHandler Defines the logic to handle the Http request.
      * @return Route Route object added to the route collection.
      */
-    public function delete(string $routePath, closure $requestHandler) : Route
+    public function delete(string $routePath, closure $requestHandler): Route
     {
         return $this->addRoute($routePath, $requestHandler, 'DELETE');
     }
 
     /**
-     * Appends a route path for the specified Http request method and its request handler.
-     * @param string $routePath Url route path.
+     * Appends a route path for the specified request method and its request handler.
+     * @param string $routePath Route path.
      * @param Closure $requestHandler Route handler.
      * @param string $method Request method.
-     * @return Route The route object that was appended.
+     * @return Route The route object added to the route collection.
      */
-    public function addRoute(string $routePath, closure $requestHandler, string $method) : Route
+    public function addRoute(string $routePath, closure $requestHandler, string $method): Route
     {
         $route = new Route($routePath, $requestHandler, $method);
 
@@ -166,14 +167,14 @@ class Router extends stdClass
     }
 
     /**
-     * Appends the request handler against the specified Http request method.
-     * @param string $requestMethod Http request method.
+     * Appends the request handler against the specified request method.
+     * @param string $requestMethod Request method.
      * @param Closure $requestHandler Defines the logic to handle the request.
      * @return Route The added Route object.
      */
-    public function requestHandler($requestMethod, $requestHandler) : Route
+    public function requestHandler(string $requestMethod, closure $requestHandler): Route
     {
-        $route = new Route('', $requestHandler, $requestMethod);
+        $route = new Route(null, $requestHandler, $requestMethod);
 
         $this->requestHandlers[$requestMethod] = $route;
 
@@ -185,7 +186,7 @@ class Router extends stdClass
      * @param Closure $handler Defines the initialization logic.
      * @return Router Current instance.
      */
-    public function initialize($handler) : Router
+    public function initialize(closure $handler): Router
     {
         $this->initializers[] = $handler;
 
@@ -197,7 +198,7 @@ class Router extends stdClass
      * @param Closure $handler Defines the middleware logic.
      * @return Router Current instance.
      */
-    public function middleware($handler) : Router
+    public function middleware(closure $handler): Router
     {
         $this->middleware[] = $handler;
 
@@ -209,7 +210,7 @@ class Router extends stdClass
      * @param Closure $errorHandler Defines the error handling logic.
      * @return Router Current instance.
      */
-    public function errorHandler($errorHandler) : Router
+    public function errorHandler(closure $errorHandler): Router
     {
         $this->errorHandlers[] = $errorHandler;
 
@@ -221,7 +222,7 @@ class Router extends stdClass
      * @param Closure $handler Defines the destruction logic.
      * @return Router Current instance.
      */
-    public function destructor($handler) : Router
+    public function destructor(closure $handler): Router
     {
         $this->destructors[] = $handler;
 
@@ -233,7 +234,7 @@ class Router extends stdClass
      * @param mixed $service Service to be injected.
      * @return Router Current instance.
      */
-    public function service($service) : Router
+    public function service($service): Router
     {
         $this->services[] = $service;
 
@@ -294,34 +295,30 @@ class Router extends stdClass
     private function serviceDestructor()
     {
         foreach ($this->services as $destructor)
-            if ($destructor instanceof  DestructorInterface)
+            if ($destructor instanceof DestructorInterface)
                 $destructor->destruct();
     }
 
     /**
-     * Executes the request handler for a specific Http request method defined against a route path.
+     * Executes the request handler for a specific request method defined against a route path.
      * The route path is provided through the $routePath parameter.
-     * <p>
+     *
      * Triggers error if the route handler for the specified route handler against the Http request method is not
      * defined.
-     * </p>
-     * <p>
+     *
      * The execution of every request handler is preceded by the execution of middleware handlers in chronological
      * order.
-     * </p>
-     * <p>
+     *
      * The execution of defined middleware is followed by the pre handlers for the request handler.
-     * </p>
-     * <p>
+     *
      * This is followed by the execution of request handler.
-     * </p>
-     * <p>
+     *
      * The execution of post handlers for the request handler continues after the request handler.
-     * </p>
+     *
      * @param string $requestMethod The Http request method.
      * @param boolean|string $routePath Url route path against which the request handler logic is to be executed.
      */
-    public function route($requestMethod, $routePath)
+    public function route(string $requestMethod, $routePath)
     {
         try {
             if (!array_key_exists($routePath, $this->routes))
@@ -329,15 +326,13 @@ class Router extends stdClass
 
             /** @var Route $route for the corresponding Http request method. */
             $route = $this->routes[$routePath];
-            if ($requestMethod != $route->httpVerb())
+            if ($requestMethod != $route->method())
                 throw new Exception('Route does not exist for the Http request method!', 404);
 
             $this->processRequest($route);
-        }
-        catch (Exception $ex) {
+        } catch (Exception $ex) {
             $this->catchBlock($ex);
-        }
-        finally {
+        } finally {
             $this->finallyBlock();
         }
     }
@@ -347,7 +342,7 @@ class Router extends stdClass
      * Triggers error if the router is not configured as a script.
      * @param string $requestMethod The Http request method.
      */
-    public function dispatch($requestMethod)
+    public function dispatch(string $requestMethod)
     {
         try {
             if (!array_key_exists($requestMethod, $this->requestHandlers))
@@ -357,11 +352,9 @@ class Router extends stdClass
             $requestHandler = $this->requestHandlers[$requestMethod];
 
             $this->processRequest($requestHandler);
-        }
-        catch (Exception $ex) {
+        } catch (Exception $ex) {
             $this->catchBlock($ex);
-        }
-        finally {
+        } finally {
             $this->finallyBlock();
         }
     }
@@ -371,16 +364,17 @@ class Router extends stdClass
      * @param $filename string Path to the view file
      * @return mixed The value returned from the view (if any).
      */
-    public function view($filename) {
+    public function view(string $filename)
+    {
         return require $filename;
     }
 
     /**
      * Executes the provided handlers in chronological order.
-     * @param Closure[] $handlers representing the handlers to be executed.
+     * @param array<closure> $handlers representing the handlers to be executed.
      * @param string $logicZone Hint for debugging.
      */
-    private function executeHandlers($handlers, $logicZone)
+    private function executeHandlers(array $handlers, string $logicZone)
     {
         /** @var Closure $handler */
         foreach ($handlers as $handler)
@@ -391,7 +385,7 @@ class Router extends stdClass
      * Executes the pipeline against an Http request.
      * @param Route $route object containing the request processing logic.
      */
-    private function processRequest($route)
+    private function processRequest(Route $route)
     {
         /** Execute all the initialization logic in chronological order. */
         $this->executeHandlers($this->initializers, 'initializers');
@@ -422,7 +416,7 @@ class Router extends stdClass
      * Executes the common logic for the exception catch block.
      * @param Exception $ex Represents the exception raised.
      */
-    private function catchBlock($ex)
+    private function catchBlock(Exception $ex)
     {
         foreach ($this->errorHandlers as $errorHandler)
             $errorHandler($ex);
