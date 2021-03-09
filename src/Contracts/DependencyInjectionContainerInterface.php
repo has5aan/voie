daf0349c1d69e0711bb066a7454f5165d3795add
has5aan <has5aan@outlook.com>
2021-03-09 19:53:17 +0100

    Dependency Injection Container Interface

--- src/Contracts/DependencyInjectionContainerInterface.php
+++ src/Contracts/DependencyInjectionContainerInterface.php
@@ -8,8 +8,8 @@ interface DependencyInjectionContainerInterface
 {
     /**
      * Returns an instance of the specified class.
-     * @param string $class Name of the class.
+     * @param string $name Name of the class.
      * @return mixed Instance of the provided class.
      */
-    public function create(string $class);
+    public function resolve(string $name);
 }
